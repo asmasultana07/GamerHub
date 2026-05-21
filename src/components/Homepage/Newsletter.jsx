3a425974e8882b352ac16508c60f51b3e81fb1a8
asmasultana07 <ab.asma1084@gmail.com>
2026-05-22 00:27:38 +0600

    added newsletter

--- src/components/Homepage/Newsletter.jsx
+++ src/components/Homepage/Newsletter.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Newsletter = () => {
   return (
-    <div className="w-full my-bg text-white  rounded-3xl p-8 md:p-12 relative overflow-hidden shadow-2xl">
-      <div className="max-w-2xl mx-auto text-center relative z-10">
+    <div className="my-10 w-full my-bg text-white  rounded-3xl p-8 md:p-12 relative overflow-hidden shadow-2xl">
+      <div className="mb-6 max-w-2xl mx-auto text-center relative z-10">
         <span className="text-purple-500 font-bold tracking-widest text-xs uppercase bg-purple-500/10 px-3 py-1 rounded-full">
           Stay Updated
         </span>
@@ -15,9 +15,12 @@ const Newsletter = () => {
           discount codes, and new game releases.
         </p>
       </div>
-      <form className="flex flex-col sm:flex-row gap-3">
-        <input type="email" name="email" 
-        className="w-full bg-transparent rounded-3xl" />
+      <form className="flex flex-col sm:flex-row gap-3 max-w-md mx-auto">
+        <input type="email" name="email" placeholder="Enter your email address"
+        className="w-full text-sm text-white bg-transparent rounded-md border border-slate-300 px-5 py-3 focus:outline-none placeholder-slate-500 transition" />
+      <button className="bg-base-200 hover:bg-base-100 font-semibold px-6 py-3 rounded-md transition  whitespace-nowrap ">
+        Subscribe
+      </button>
       </form>
     </div>
   );
